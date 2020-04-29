7285234aeb561a4373d3c9713cc3e62025a552e3
Aswin A <dev.aswin.a@gmail.com>
2020-04-29 21:17:00 +0530

    WIP: Selecting profile pic

--- lib/components/LargeAvatar.dart
+++ lib/components/LargeAvatar.dart
@@ -1,57 +1,98 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 
 class LargeAvatar extends StatelessWidget {
   final bool editMode;
-  final TextEditingController nameController;
   final bool autoFocus;
-  final void Function (String) onNameChanged;
-  final void Function (String) onNameSubmitted;
 
-  LargeAvatar({@required this.nameController, this.editMode = false, this.autoFocus = false, this.onNameChanged, this.onNameSubmitted});
+  final TextEditingController nameController;
+  final void Function(String) onNameChanged;
+  final void Function(String) onNameSubmitted;
+
+  final ImageProvider imageProvider;
+  final void Function(File) onImageUpdated;
+
+  LargeAvatar({
+    this.editMode = false,
+    this.autoFocus = false,
+    this.nameController,
+    this.onNameChanged,
+    this.onNameSubmitted,
+    this.imageProvider =
+        const AssetImage('assets/images/defaultAvatarLarge.png'),
+    this.onImageUpdated,
+  });
+
+  void onTapAvatar(BuildContext context) {
+    FocusScope.of(context).unfocus(focusPrevious: true);
+    ImagePicker.pickImage(source: ImageSource.gallery).then((imageFile) {
+      ImageCropper.cropImage(
+          sourcePath: imageFile.path,
+          aspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1),
+          cropStyle: CropStyle.circle,
+          androidUiSettings: AndroidUiSettings(
+            toolbarColor: Theme.of(context).scaffoldBackgroundColor,
+            toolbarWidgetColor: Colors.white,
+            cropFrameColor: Colors.transparent,
+            showCropGrid: false,
+            hideBottomControls: true,
+          )).then((imageFile) {
+        this.onImageUpdated(imageFile);
+      });
+    });
+  }
+
+  
 
   @override
   Widget build(BuildContext context) {
+    print('Large Avatar Rebuilt: ${this.nameController.text}');
     return Container(
       height: 220,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Stack(
-            alignment: Alignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 66,
-                backgroundColor: Colors.white,
-                child: CircleAvatar(
-                  radius: 62,
-                  backgroundColor: Colors.transparent,
-                  backgroundImage:
-                      AssetImage('assets/images/defaultAvatarLarge.png'),
+          InkWell(
+            onTap: this.editMode ? () => this.onTapAvatar(context) : null,
+            child: Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 66,
+                  backgroundColor: Colors.white,
+                  child: CircleAvatar(
+                    radius: 62,
+                    backgroundColor: Colors.transparent,
+                    backgroundImage: this.imageProvider,
+                  ),
                 ),
-              ),
-              this.editMode
-                  ? Positioned(
-                      top: 110,
-                      left: 52,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(3),
-                        child: Container(
-                          color: Colors.black.withOpacity(0.7),
-                          padding:
-                              EdgeInsets.symmetric(vertical: 2, horizontal: 4),
-                          child: Text(
-                            'Edit',
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
+                this.editMode
+                    ? Positioned(
+                        top: 110,
+                        left: 52,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(3),
+                          child: Container(
+                            color: Colors.black.withOpacity(0.7),
+                            padding: EdgeInsets.symmetric(
+                                vertical: 2, horizontal: 4),
+                            child: Text(
+                              'Edit',
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    )
-                  : Container(),
-            ],
+                      )
+                    : Container(),
+              ],
+            ),
           ),
           ClipRRect(
             borderRadius: BorderRadius.circular(10),
@@ -59,16 +100,15 @@ class LargeAvatar extends StatelessWidget {
               width: MediaQuery.of(context).size.width * 0.7,
               child: TextField(
                 controller: this.nameController,
-                onChanged: onNameChanged,
-                onSubmitted: onNameSubmitted,
+                onChanged: this.onNameChanged,
+                onSubmitted: this.onNameSubmitted,
                 cursorColor: Colors.white,
                 decoration: InputDecoration(
-                  border: InputBorder.none,
-                  filled: this.editMode,
-                  fillColor: Colors.white.withOpacity(0.3),
-                  hintText: 'Name',
-                  hintStyle: Theme.of(context).textTheme.display1
-                ),
+                    border: InputBorder.none,
+                    filled: this.editMode,
+                    fillColor: Colors.white.withOpacity(0.3),
+                    hintText: 'Name',
+                    hintStyle: Theme.of(context).textTheme.display1),
                 showCursor: true,
                 style: Theme.of(context).textTheme.display3,
                 textAlign: TextAlign.center,
--- lib/pages/UserSettings/UserSettings.dart
+++ lib/pages/UserSettings/UserSettings.dart
@@ -1,4 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -12,22 +13,48 @@ class UserSettings extends StatelessWidget {
   static const pageTitle = 'User Settings';
 
   Widget build(BuildContext context) {
+    print('User Settings rebuilt');
     final routeArgs = ModalRoute.of(context).settings.arguments as Map;
     return Material(
       type: MaterialType.canvas,
       color: Theme.of(context).scaffoldBackgroundColor,
       child: SafeArea(
         minimum: EdgeInsets.symmetric(horizontal: 15),
-        child: _UserSettingsContent(routeArgs: routeArgs),
+        child: _UserSettingsStreamContent(routeArgs: routeArgs),
       ),
     );
   }
 }
 
-class _UserSettingsContent extends StatefulWidget {
+class _UserSettingsStreamContent extends StatelessWidget {
   final routeArgs;
 
-  _UserSettingsContent({@required this.routeArgs});
+  _UserSettingsStreamContent({@required this.routeArgs});
+
+  @override
+  Widget build(BuildContext context) {
+    print('User Settings Stream rebuilt');
+    return StreamBuilder<Object>(
+        stream: Firestore.instance
+            .collection('users')
+            .document(Constants().currentUserId)
+            .snapshots(),
+        builder: (context, snapshot) {
+          String userName;
+          if (snapshot.connectionState == ConnectionState.active) {
+            final documentSnapshot = snapshot.data as DocumentSnapshot;
+            userName = documentSnapshot.data['name'];
+          }
+          return _UserSettingsContent(routeArgs: routeArgs, userName: userName);
+        });
+  }
+}
+
+class _UserSettingsContent extends StatefulWidget {
+  final routeArgs;
+  final String userName;
+
+  _UserSettingsContent({@required this.routeArgs, @required this.userName});
 
   @override
   _UserSettingsContentState createState() => _UserSettingsContentState();
@@ -35,7 +62,17 @@ class _UserSettingsContent extends StatefulWidget {
 
 class _UserSettingsContentState extends State<_UserSettingsContent> {
   var editMode = false;
+  String name;
   final nameController = TextEditingController(text: '');
+  ImageProvider imageProvider =
+      AssetImage('assets/images/defaultAvatarLarge.png');
+
+  
+  @override
+  void didUpdateWidget(_UserSettingsContent oldWidget) {
+    this.nameController.text = widget.userName;
+    super.didUpdateWidget(oldWidget);
+  }
 
   void leftButtonOnTap() {
     if (this.editMode) {
@@ -64,54 +101,56 @@ class _UserSettingsContentState extends State<_UserSettingsContent> {
     }
   }
 
+  void onImageUpdated(File imageFile) {
+    this.setState(() {
+      this.imageProvider = FileImage(imageFile);
+    });
+  }
+
+  void onNameChanged (String name) {
+    print('Changed: $name');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<Object>(
-      stream: Firestore.instance
-          .collection('users')
-          .document(Constants().currentUserId)
-          .snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.active) {
-          final documentSnapshot = snapshot.data as DocumentSnapshot;
-          this.nameController.text = documentSnapshot.data['name'];
-        }
-        return Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            TitleBar(
-              UserSettings.pageTitle,
-              showLeftButton: true,
-              showLeftChevron: !this.editMode,
-              leftButtonTitle:
-                  this.editMode ? 'Cancel' : widget.routeArgs['fromPage'],
-              leftButtonOnTapFn: this.leftButtonOnTap,
-              showRightButton: true,
-              rightButtonTitle: this.editMode ? 'Save' : 'Edit',
-              rightButtonOnTapFn: () => (this.rightButtonOnTap()),
-            ),
-            LargeAvatar(
-              nameController: this.nameController,
-              editMode: this.editMode,
-            ),
-            Expanded(
-              child: Container(
-                alignment: Alignment.bottomCenter,
-                padding: EdgeInsets.only(bottom: 20),
-                child: !this.editMode
-                    ? FlatButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Logout',
-                          style: TextStyles.flatButtonStyle,
-                        ),
-                      )
-                    : Container(),
-              ),
-            )
-          ],
-        );
-      },
+    print('User Settings Content rebuilt');
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        TitleBar(
+          UserSettings.pageTitle,
+          showLeftButton: true,
+          showLeftChevron: !this.editMode,
+          leftButtonTitle:
+              this.editMode ? 'Cancel' : widget.routeArgs['fromPage'],
+          leftButtonOnTapFn: this.leftButtonOnTap,
+          showRightButton: true,
+          rightButtonTitle: this.editMode ? 'Save' : 'Edit',
+          rightButtonOnTapFn: () => (this.rightButtonOnTap()),
+        ),
+        LargeAvatar(
+          editMode: this.editMode,
+          nameController: this.nameController,
+          onNameChanged: this.onNameChanged,
+          imageProvider: this.imageProvider,
+          onImageUpdated: this.onImageUpdated,
+        ),
+        Expanded(
+          child: Container(
+            alignment: Alignment.bottomCenter,
+            padding: EdgeInsets.only(bottom: 20),
+            child: !this.editMode
+                ? FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Logout',
+                      style: TextStyles.flatButtonStyle,
+                    ),
+                  )
+                : Container(),
+          ),
+        )
+      ],
     );
   }
 }
