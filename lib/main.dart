7b8f9be3eac82e44671fe76d9860ff559f61d841
Aswin A <dev.aswin.a@gmail.com>
2020-05-13 13:42:54 +0530

    Refactor: App color,  icon size and analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 import './pages/Splash/Splash.dart';
 import './pages/Login/Login.dart';
@@ -10,7 +12,18 @@ import './pages/UserSettings/UserSettings.dart';
 import './styles/textThemes.dart';
 import './constants.dart';
 
-void main() => runApp(CaringCircle());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
+  Crashlytics.instance.enableInDevMode = true;
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+
+  runApp(CaringCircle());
+}
 
 class CaringCircle extends StatelessWidget {
   @override
@@ -18,7 +31,7 @@ class CaringCircle extends StatelessWidget {
     return MaterialApp(
       title: 'Caring Circle',
       theme: ThemeData(
-        scaffoldBackgroundColor: Color(0xFF0098BC),
+        scaffoldBackgroundColor: Color(0xFF044B7F) ,
         textTheme: textTheme,
       ),
       home: FutureBuilder(
